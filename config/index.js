643656b41f79ebf06e75a5c3bee78d3066da7fcb
Ray Quijada <ray.quijada@jway.com>
2021-10-08 17:38:54 +0800

    [Config] add google config

---
+++ config/index.js
@@ -0,0 +1,8 @@
+export default {
+    issuer: 'https://accounts.google.com',
+    clientId:
+        '745545678064-c4ed9mp4dhk24eu6kghvshvta7nkkeuc.apps.googleusercontent.com',
+    redirectUrl:
+        'com.googleusercontent.apps.745545678064-c4ed9mp4dhk24eu6kghvshvta7nkkeuc:/oauth2redirect/google',
+    scopes: ['openid', 'profile'],
+}
